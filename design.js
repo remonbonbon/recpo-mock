70960bedf11b6d4fd62b8e19225038f5950f4dee
remonbonbon <remonbonbon@users.noreply.github.com>
2024-07-10 16:58:33 +0900

    WIP

--- design.js
+++ design.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      articles: _(ARTICLES).shuffle().take(2).value(),
+      articles: _(ARTICLES).shuffle().take(4).value(),
     };
   },
 }).mount("#app");
